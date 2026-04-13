d780e2ad490ccd3cf545434782264dd899aa0370
barcagt <diaztito112@gmail.com>
2026-04-13 11:20:15 -0600

    Actualizar problema solucion

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5cbx7-iqf!*khf&qeav9##tnhozxoe0*qgv9!tjn^2w437(=)n'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5cbx7-iqf!*khf&qeav9##tnhozxoe0*qgv9!tjn^2w437(=)n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-if os.environ.get('DATABASE_URL'):
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
+            default=DATABASE_URL,
             conn_max_age=600,
+            conn_health_checks=True,
         )
     }
 else:
