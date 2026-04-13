7d1bfaf59fa9b53063b2a2abffab6502f86c7b5f
barcagt <diaztito112@gmail.com>
2026-04-13 12:16:25 -0600

    usuario

--- core/apps.py
+++ core/apps.py
@@ -3,3 +3,18 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db import OperationalError, ProgrammingError
+
+        User = get_user_model()
+        try:
+            if not User.objects.filter(is_superuser=True).exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@example.com',
+                    password='admin123',
+                )
+        except (OperationalError, ProgrammingError):
+            pass
