608b6211f9258c8e7f996e30ecf15a2c7ab8fbd4
barcagt <diaztito112@gmail.com>
2026-04-13 11:09:43 -0600

    Actualizar vista de error

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5cbx7-iqf!*khf&qeav9##tnhozxoe0*qgv9!tjn^2w437(=)n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
